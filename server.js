28cdad97c59e1b4e2905e7c6f36c740c6819ac81
Evolan <yon_baermund@hotmail.com>
2023-02-27 16:33:49 -0500

    push after tutoring session, worked on the models and some routes, made pertinent connections.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection.js');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+// we want the force as false unless we make any major changes to the models. true recreates the tables everytime and seed fills the tables.
+sequelize.sync({force: false}).then(function(err){
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
+
